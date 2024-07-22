afba343dbfab0345368b4091862a1b3380b26236
s1u2 <165137940+08april06@users.noreply.github.com>
2024-07-22 09:22:03 +0900

    Create ForTest.java

---
+++ ForTest.java
@@ -0,0 +1,9 @@
+public class ForTest_1 {
+	public static void main(String args[]) {
+		int n = 0;
+		for (int i = 1; i <= 10; i++) {
+			n+=i;
+		}
+		System.out.printf("1부터\t10까지의\t합은:\t"+n);
+	}
+}
