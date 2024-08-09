3c04d9480af4fefdef61e937e648e124a96b0ded
s1u2 <165137940+08april06@users.noreply.github.com>
2024-08-09 18:41:50 +0900

    Create ForTest_2.java

---
+++ ForTest_2.java
@@ -0,0 +1,11 @@
+public class ForTest_2 {
+	public static void main(String args[]) {
+		int order = 1;
+		int reverse = 10;
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(order + "\t" + reverse);
+				order++;
+				reverse--;
+		}
+	}
+}
